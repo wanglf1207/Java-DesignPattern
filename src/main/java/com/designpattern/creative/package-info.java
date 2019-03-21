6f513dcad0b448870de7dc27e17245f0b0a9d20d
wanglf1207 <wanglf1207@163.com>
2019-03-21 15:53:16 +0800

    修改乱码

--- src/main/java/com/designpattern/creative/package-info.java
+++ src/main/java/com/designpattern/creative/package-info.java
@@ -1,8 +1,8 @@
 /**
- * ������ģʽ
+ * 创建型模式
  */
 /**
  * @author wanglf1207
- * �����֣���������ģʽ�����󹤳�ģʽ������ģʽ��������ģʽ��ԭ��ģʽ��
+ * 共五种：工厂方法模式、抽象工厂模式、单例模式、建造者模式、原型模式。
  */
 package com.designpattern.creative;
