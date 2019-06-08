827e053ff8606ea93bd674670c2aee28338c40db
wanglf1207 <wanglf1207@163.com>
2019-06-08 19:09:00 +0800

    修改单例模式

--- src/main/java/com/designpattern/creative/factory/commonfactory/SenderFactory.java
+++ src/main/java/com/designpattern/creative/factory/commonfactory/SenderFactory.java
@@ -1,21 +1,23 @@
 package com.designpattern.creative.factory.commonfactory;
 
-public class SenderFactory {
+ class SenderFactory {
 	
 	/**
 	 * @param protocolType 协议类型
 	 */
-	public Sender produce(String protocolType) {
-		
-		if(protocolType.equals("TCP")) {
-			return new TCPSender();
-		} else if(protocolType.equals("FTP")) {
-			return new FTPSender();
-		} else if(protocolType.equals("HTTP")) {
-			return new HTTPSender();
-		} else {
-			System.out.println("请输入合适的类型");
-			return null;
+	 Sender produce(String protocolType) {
+
+		switch (protocolType) {
+			case "TCP":
+				return new TCPSender();
+			case "FTP":
+				return new FTPSender();
+			case "HTTP":
+				return new HTTPSender();
+			default:
+				System.out.println("请输入合适的类型");
+				break;
 		}
+		return  null;
 	}
 }
